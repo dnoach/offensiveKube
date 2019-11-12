90edd9eb2ea51992571a12f27aad0162578ab96f
Dev01 <dnoach@gmail.com>
2019-11-12 12:12:10 -0500

    Fixed metasploit build

--- offensiveKube.py
+++ offensiveKube.py
@@ -1,10 +1,13 @@
 import os
 import sys
+import docker
+import inquirer
 
 templates = ['nmap', 'metasploit', 'other']
+client = docker.from_env()
 
 
-def createcontainer():
+def selectcontainer():
     select = ""
 
     print ("Select template from the options below:")
@@ -14,6 +17,26 @@ def createcontainer():
 
     while select not in templates:
             select = input()
+    return select
+
+def buildcontainer(img):
+    (
+    os.system("docker build -t offensive:"+image+" "+image+"/.")
+    )
+
+def runcontainer(name):
+    (
+    client.containers.run("offensive:"+name, detach=True,
+                                            stdin_open = True,
+                                                tty = True)
+    )
 
 
-createcontainer()
+
+
+image = selectcontainer()
+buildcontainer(image)
+
+#runcontainer("nmap")
+#client.containers.get("nmap")
+os.system("docker run -it offensive:"+image)
